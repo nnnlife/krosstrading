8b5a1b2503204992db2a157b1b457d217385a5e6
yongwoo1.park <yongwoo1.park@lge.com>
2021-03-29 10:46:25 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='krosstrading',
+      version='0.1',
+      description='Helloworld project for CI and Coverage',
+      url='https://github.com/nnnlife/krosstrading',
+      author='Yongwoo Park',
+      author_email='nnnlife@gmail.com',
+      license='MIT',
+      packages=['krosstrading'],
+      zip_fase=False)
